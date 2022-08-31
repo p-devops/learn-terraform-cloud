c5f82f178030c8d85b953da371dbadab98a80d95
Peter Leung <pleung@hashicorp.com>
2022-08-31 15:48:11 -0400

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "pl-org"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
